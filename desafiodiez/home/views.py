07e28a3c1cffdd0bf587f4c3c1becc5dd5d7e381
Gustavo <110218589+gustavopalaciosTecno@users.noreply.github.com>
2023-06-28 08:12:21 -0300

    Update views.py

--- desafiodiez/home/views.py
+++ desafiodiez/home/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse(f"<h1>¡Hola Lucas!</h1> <h2> <b>Este es nuestro home :D </h2> </b> <br> Integrantes del grupo <b><u>Pythoneers</u></b>: <br> -Yrala Fabian <br>-Morinigo Pedro <br>-Sosa Fogar Yamila Noel <br>-Palacios Meyer Néstor Gustavo <br>-Cuquejo Daira <br>-Bazan Julio Pablo Federico <br>-Gonzalez Juan Ignacio")
+
+def about(request):
+    return HttpResponse("<h1>Grupo Pyhtoneers formado en el año 2023 durante el inforamtorio</h1>")
