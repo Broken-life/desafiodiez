273f1ae9347f42b6f982ff9b33b3d412f2d1ebfd
FedeBazan <juliopablofedericobazan@gmail.com>
2023-06-28 09:45:59 -0300

    Fede Bazan
    Reordenamiento de estructura HTML

--- desafiodiez/home/views.py
+++ desafiodiez/home/views.py
@@ -4,7 +4,18 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse(f"<h1>¡Hola Lucas!</h1> <h2> <b>Este es nuestro home :D </h2> </b> <br> Integrantes del grupo <b><u>Pythoneers</u></b>: <br> -Yrala Fabian <br>-Morinigo Pedro <br>-Sosa Fogar Yamila Noel <br>-Palacios Meyer Néstor Gustavo <br>-Cuquejo Daira <br>-Bazan Julio Pablo Federico <br>-Gonzalez Juan Ignacio")
+    return HttpResponse('''<h1>¡Hola Lucas!</h1> 
+    <h2><b>Este es nuestro home :D </b></h2>
+    <p>Integrantes del grupo <b><u>Pythoneers</u>:</b></p> 
+    <ul>
+        <li>Yrala Fabian</li>
+        <li>Morinigo Pedro</li>
+        <li>Sosa Fogar Yamila Noel</li>
+        <li>Palacios Meyer Néstor Gustavo</li>
+        <li>Cuquejo Daira</li>
+        <li>Bazan Julio Pablo Federico</li>
+        <li>Gonzalez Juan Ignacio</li>
+    </ul>''')
 
-def about(request):
-    return HttpResponse("<h1 style='color:red';>Grupo Pyhtoneers formado en el año 2023 durante el inforamtorio</h1>")
+def about(request): #buscar forma de linkear css en django
+    return HttpResponse("<h1 style='color:red';>Grupo Pyhtoneers formado en el año 2023 durante el Informatorio</h1>")
